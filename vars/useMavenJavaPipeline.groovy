bf747f4bb7795f52c04e278d59bf52fd39337ace
Luther Dang <huucanhptit@gmail.com>
2023-03-19 16:29:05 +0700

    Fixed pipeline

--- vars/useMavenJavaPipeline.groovy
+++ vars/useMavenJavaPipeline.groovy
@@ -1,34 +1,33 @@
 #!/usr/bin/env groovy
 
-// import org.canhdevsecops.MavenJavaPipeline
+import org.canhdevsecops.MavenJavaPipeline
 
 def call(String javaVersion) {
-    // def pipeline = new MavenJavaPipeline(this)
+    def pipeline = new MavenJavaPipeline(this)
 
     pipeline {
-        stages {
-            stage('Build') {
-                agent(
-                    docker {
-                        image "3.8-openjdk-${javaVersion}-slim"
-                    }
-                )
-                steps {
-                    sh 'echo "Pipeline running"'
-                    // pipeline.build
-                }
-            }
-        }
+        agent any
 
         stages {
             stage('Build') {
-                agent(
+                agent {
                     docker {
-                        image "3.8-openjdk-${javaVersion}-slim"
+                        image "maven:3.8-openjdk-${javaVersion}-slim"
                     }
-                )
+                }
                 steps {
-                    // pipeline.test
+                    pipeline.build
+                }
+            }
+
+            stage('Test') {
+                agent{
+                    docker {
+                        image "maven:3.8-openjdk-${javaVersion}-slim"
+                    }
+                }
+                steps {
+                    pipeline.test
                 }
             }
         }
